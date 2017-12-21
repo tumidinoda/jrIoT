6214d095314ae8d2a64cc1e3873606f85c2cf882
robert@Mac <robert.jonas@gmx.at>
2017-12-21 21:03:13 +0100

    Testframework
    first update

---
+++ jrIoT/Model/IoTitem.swift
@@ -0,0 +1,16 @@
+//
+//  IoTitem.swift
+//  jrIoT
+//
+//  Created by Robert Jonas on 21.12.17.
+//  Copyright © 2017 jr-soft. All rights reserved.
+//
+
+import Foundation
+
+class IoTitem {
+    init (ip: String) {
+        
+    }
+}
+
