ea76854120110a3c938f632958bd5493605e7451
robert@Mac <robert.jonas@gmx.at>
2017-12-21 19:56:50 +0100

    Initial Commit

---
+++ jrIoT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jrIoT
+//
+//  Created by Robert Jonas on 21.12.17.
+//  Copyright © 2017 jr-soft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
