719bd7878cf49bdee7672d99889a01ed3446944e
robert@Mac <robert.jonas@gmx.at>
2018-01-01 20:16:32 +0100

    Basic Layout finished

--- jrIoT/ViewController.swift
+++ jrIoT/ViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+ 
+    @IBOutlet weak var myTextView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
