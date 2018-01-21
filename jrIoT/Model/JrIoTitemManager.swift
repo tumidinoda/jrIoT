422895cbded01450130a722a9d82b023cbfbcaa8
robert@Mac <robert.jonas@gmx.at>
2018-01-21 19:10:26 +0100

    Merge branch 'feature/jrIoTitem' into develop

--- jrIoT/Model/JrIoTitemManager.swift
+++ jrIoT/Model/JrIoTitemManager.swift
@@ -8,45 +8,36 @@
 
 import Foundation
 
-/*
 // Singleton class JrIoTitemManager
 class JrIoTitemManager {
-    //private var itemList = [String: JrIoTitem]()
-    /*
-    private static let sharedInstance = JrIoTitemManager()
-    class var sharedManager : JrIoTitemManager {
-        return sharedInstance
-    } */
+    static let sharedInstance = JrIoTitemManager()
+    private init() {} //avoid creation of object instance
+    
+    var itemList = [JrIoTitem]()
+    weak var timer: Timer?
+    var timerCounter=0
+    
     //----------------------------------------------------------------------------------------------------
-   func add (iotItem: JrIoTitem) {
-     // self.itemList[iotItem.mqttBrokerHost] = iotItem
-   }
-    
+    func printItems() {
+        for item in itemList {
+            print("Broker host: \(item.mqttBrokerHost) Alive: \(item.alive)")
+        }
+    }
+    //----------------------------------------------------------------------------------------------------
+    func startKeepAliveTimer(interval: TimeInterval) {  //in seconds  e.g.: 0.1 means 100 msec
+        timer?.invalidate()   // just in case we had existing `Timer`, `invalidate`
+        timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: true)
+                { [weak self] _ in self?.callbackKeepAlive() }
+    }
+    //----------------------------------------------------------------------------------------------------
+    func stopKeepAliveTimer() {
+        timer?.invalidate()
+    }
+    //----------------------------------------------------------------------------------------------------
+    func callbackKeepAlive() {
+        timerCounter+=1
+        print("Keep alive")
+    }
 }
-*/
 
-class JrIoTitemManager {
- 
-    func add (iotItem: JrIoTitem) {
-        print ("IoTitem added \(iotItem)")
-        
-    }
-    func add (item: String) {
-        print ("IoTitem added \(item)")
-    }
-    
-}
 
-/*
-class anotherTest {
-    init() {
-        let test=myTestClass()
-        let myIoTitem = JrIoTitem(host: "10.0.0.14")
-        
-        test.add(iotItem: myIoTitem)
-    }
-    
-    
-    
-}
-*/
--- jrIoTTests/jrIoTitemManagerTests.swift
+++ jrIoTTests/jrIoTitemManagerTests.swift
@@ -5,14 +5,11 @@
 //  Created by Robert Jonas on 19.01.18.
 //  Copyright © 2018 jr-soft. All rights reserved.
 //
-
 import XCTest
 @testable import jrIoT
-//import jrIoT
-
 
 class jrIoTitemManagerTests: XCTestCase {
-
+    let myItemMgr = JrIoTitemManager.sharedInstance
     
     //----------------------------------------------------------------------------------------------------
     override func setUp() {
@@ -27,28 +24,41 @@ class jrIoTitemManagerTests: XCTestCase {
     
     //----------------------------------------------------------------------------------------------------
     func testAddIoTitem() {
-
+        let myIoThost = "10.0.0.14"
+        let myIoTitem = JrIoTitem(host: myIoThost)
         
-        let test=JrIoTitemManager()
-        var myIoTitem: JrIoTitem
-        myIoTitem = JrIoTitem(host: "10.0.0.14")
+        myItemMgr.itemList.append(myIoTitem)
+        XCTAssertEqual(myItemMgr.itemList.count, 1)
+        XCTAssertEqual(myItemMgr.itemList[0].mqttBrokerHost, myIoThost)
         
-        test.add(iotItem: myIoTitem)
-        test.add(item: "Hallo")
-        
-  
-        
-        
-        
-        
-        
-        //myIoTitemManager.add(iotItem: myIoTitem)
-        
-        //myIoTitemManager.add(iotItem: myIoTitem)
-        //myIoTitemManager.add(iotItem: 5)
     }
     
-    
-    
-    
+    //----------------------------------------------------------------------------------------------------
+    func testKeepAlive() {
+        let myIoThost = "10.0.0.15"  //non existing MqttBroker
+        let myIoTitem = JrIoTitem(host: myIoThost)
+        
+        myItemMgr.itemList.append(myIoTitem)
+        XCTAssertEqual(myItemMgr.itemList.count, 2)
+        
+        myItemMgr.startKeepAliveTimer(interval: 5)
+        
+        //wait 60 seconds
+        while myItemMgr.timerCounter < 12 {
+            wait(waitTime: 12)
+            print("Wait timer finished")
+        }
+        print("testKeepAliveFinished")
+
+        
+        //stop keepAliveTimer
+        myItemMgr.stopKeepAliveTimer()
+        XCTAssertNil(myItemMgr.timer)
+        
+    }
+    //----------------------------------------------------------------------------------------------------
+    func wait(waitTime: TimeInterval) //in seconds  e.g.: 0.1 means 100 msec
+    {
+        RunLoop.current.run(mode: RunLoopMode.defaultRunLoopMode, before: NSDate(timeIntervalSinceNow: waitTime) as Date)
+    }
 }
